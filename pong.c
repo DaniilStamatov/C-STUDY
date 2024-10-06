d203a778a4443bc528118aaa851bcc7814f16a05
Daniil <danstam63@gmail.com>
2024-10-06 17:39:26 +0300

    pong.c 2

--- pong.c
+++ pong.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
-
+#include <conio.h>
+#include <windows.h>
 const int WIDTH = 80;
 const int HEIGHT = 25;
 const int ROCKET_WIDTH = 3;
 const int BALL_SIZE = 1;
-const int OFFSET = 1;
-void clearScreen() {
-    printf("\e[1;1H\e[2J");
+const int OFFSET = 3;
+const int SCORE = 21;
+void clear_screen() {
+
     printf("\033[H");
 }
 
-void draw(int left_rocket, int right_rocket, int ball_x, int ball_y) {
-    clearScreen();
+int check_for_rocket(int y, int rocket) {
+    return y == rocket || y == rocket - 1 || y == rocket + 1;
+}
+
+void draw(int left_rocket, int right_rocket, int ball_x, int ball_y, int score_left, int score_right) {
+    clear_screen();
+    printf("\t\t%2d\t\t\t\t\t\t%2d\n", score_left, score_right);
+
     for (int i = 0; i < WIDTH; i++) printf("=");
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            if (x == ball_x && y == ball_y) {
+            if ((x == ball_x && y == ball_y)) {
                 printf("*");
             }
-
-            if (x == OFFSET && (y == left_rocket || y == left_rocket + 1 || y == left_rocket - 1) && left_rocket > 1 && left_rocket < HEIGHT - 1) {
+            if (x == OFFSET && check_for_rocket(y, left_rocket) && left_rocket > 1 && left_rocket < HEIGHT - 1) {
                 printf("|");
             }
-            else if (x == WIDTH - OFFSET && (y == right_rocket || y == right_rocket + 1 || y == right_rocket - 1) && right_rocket > 1 && right_rocket < HEIGHT - 1) {
-                if (ball_y == y) {
+            else if (x == WIDTH - OFFSET && check_for_rocket(y, right_rocket) && right_rocket > 1 && right_rocket < HEIGHT - 1) {
+                if (ball_y == y && ball_x < WIDTH - OFFSET) {
                     printf("\b");
                 }
                 printf("|");
             }
             else {
                 printf(" ");
-            }
 
+            }
 
         }
         printf("\n");
@@ -40,16 +47,7 @@ void draw(int left_rocket, int right_rocket, int ball_x, int ball_y) {
     for (int i = 0; i < WIDTH; i++) printf("=");
 }
 
-int move_ball_x(int direction) {
-    switch (direction) {
-    case 1: return 1;
-    case -1: return -1;
-    default:
-        return 0;
-    }
-}
-
-int move_ball_y(int direction) {
+int move_ball(int direction) {
     switch (direction) {
     case 1: return 1;
     case -1: return -1;
@@ -59,9 +57,9 @@ int move_ball_y(int direction) {
 }
 
 int calculate_direction_x(int ball_x, int ball_y, int left_rocket, int right_rocket) {
-    if ((ball_x == WIDTH - OFFSET - 1 || ball_x == OFFSET + 1) && ((ball_y == right_rocket || ball_y == right_rocket - 1 || ball_y == right_rocket + 1) ||
-        (ball_y == left_rocket || ball_y == left_rocket - 1 || ball_y == left_rocket + 1))) {
-        printf("DS");
+    if ((ball_x == WIDTH - OFFSET - 2 || ball_x == OFFSET + 1)
+        && (check_for_rocket(ball_y, right_rocket) || check_for_rocket(ball_y, left_rocket))) {
+        printf("%d", ball_x);
         return -1;
     }
     return 1;
@@ -70,11 +68,13 @@ int calculate_direction_x(int ball_x, int ball_y, int left_rocket, int right_roc
 
 int calculate_direction_y(int ball_x, int ball_y, int left_rocket, int right_rocket) {
     if (ball_y == 1 || ball_y == HEIGHT - 1) {
-        printf("DS");
         return -1;
     }
     return 1;
+}
 
+void reset(int score_left, int score_right) {
+    draw(12, 12, 40, 12, score_left, score_right);
 }
 
 int main() {
@@ -84,9 +84,14 @@ int main() {
     int right_rocket = 12;
     int direction_x = 1;
     int direction_y = 1;
-    draw(left_rocket, right_rocket, ball_x, ball_y);
-    while (1) {
-        char move = getchar();
+    int score_left = 0;
+    int score_right = 0;
+    char move = ' ';
+    draw(left_rocket, right_rocket, ball_x, ball_y, score_left, score_right);
+    while (score_left < SCORE && score_right < SCORE) {
+        if (_kbhit()) {
+            move = _getch();
+        }
         if ((move == 'A' || move == 'a') && left_rocket > 2) {
             left_rocket--;
         }
@@ -100,15 +105,43 @@ int main() {
             right_rocket++;
         }
 
-        ball_x += move_ball_x(direction_x);
-        ball_y += move_ball_y(direction_y);
-
         direction_x *= calculate_direction_x(ball_x, ball_y, left_rocket, right_rocket);
         direction_y *= calculate_direction_y(ball_x, ball_y, left_rocket, right_rocket);
+        ball_x += move_ball(direction_x);
+        ball_y += move_ball(direction_y);
+        if (ball_x == WIDTH && !check_for_rocket(ball_y, right_rocket)) {
+            score_left++;
+            left_rocket = 12;
+            right_rocket = 12;
+            ball_x = 40;
+            ball_y = 12;
+        }
+        else if (ball_x == 0 && !check_for_rocket(ball_y, left_rocket)) {
+            score_right++;
 
-        draw(left_rocket, right_rocket, ball_x, ball_y);
+            left_rocket = 12;
+            right_rocket = 12;
+            ball_x = 40;
+            ball_y = 12;
+        }
+
+        draw(left_rocket, right_rocket, ball_x, ball_y, score_left, score_right);
+        move = ' ';
+        Sleep(100);
     }
-
+    clear_screen();
+    if (score_left == SCORE) {
+        printf("Player 1 WIN");
+        for (int i = 0; i < 50; i++) {
+            printf("!");
+        }
+    }
+    else if (score_right == SCORE) {
+        printf("Player 2 WIN");
+        for (int i = 0; i < 50; i++) {
+            printf("!");
+        }
+    }
     return 0;
 }
 
